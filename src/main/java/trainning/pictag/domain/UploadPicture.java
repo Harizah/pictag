596627196889f6ead95d0857bee5c1470447a9ac
Harizah <harizah.suhaili@outlook.com>
2013-12-03 12:02:37 +0800

    change project name

--- src/main/java/trainning/pictag/domain/UploadPicture.java
+++ src/main/java/trainning/pictag/domain/UploadPicture.java
@@ -6,10 +6,78 @@
 
 package trainning.pictag.domain;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 /**
  *
  * @author User
  */
-public class UploadPicture {
+public class UploadPicture 
+{
+    private String id;
+    private Date captureDate;
+    private User owner;
+    private String imageLocation;
+    private BigDecimal latitude;
+    private BigDecimal longtitude;
+    private String remarks;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getCaptureDate() {
+        return captureDate;
+    }
+
+    public void setCaptureDate(Date captureDate) {
+        this.captureDate = captureDate;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getImageLocation() {
+        return imageLocation;
+    }
+
+    public void setImageLocation(String imageLocation) {
+        this.imageLocation = imageLocation;
+    }
+
+    public BigDecimal getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(BigDecimal latitude) {
+        this.latitude = latitude;
+    }
+
+    public BigDecimal getLongtitude() {
+        return longtitude;
+    }
+
+    public void setLongtitude(BigDecimal longtitude) {
+        this.longtitude = longtitude;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+    
     
 }
